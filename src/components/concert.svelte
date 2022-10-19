3ac2627986c1e25e351ce0b6142fc67e3291575b
u215084 <daniel.fink@sbb.ch>
2022-10-19 17:31:58 +0200

    first draft for concert section

---
+++ src/components/concert.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  export let date: Date = new Date();
+  export let titel: String = 'Titel';
+  export let start: String = 'Start';
+  export let link: string = 'https://www.midlands.ch'
+
+  const monthNames = ["JAN", "FEB", "MAR", "APR", "MAI", "JUN",
+  "JUL", "AUG", "SEP", "OKT", "NOV", "DEZ"];
+
+</script>
+
+<div class="py-3 sm:py-6 sm:px-12">
+  <div class="flex flex-row items-end space-x-6 sm:space-x-12">
+    <div class="day text-white font-cooper-heavy opacity-40">{date.getDate()}</div>
+    <div class="text-darkblue font-cooper-bold text-lg leading-6 sm:text-4xl pb-7 sm:pb-12">{titel}</div>
+  </div>
+  
+  <div class="flex flex-row items-start space-x-6 sm:space-x-12 -mt-8 sm:-mt-14">
+    <div class="month text-white font-cooper-heavy opacity-40">{monthNames[date.getMonth()]}</div>
+    <div class="flex flex-col justify-between pt-2 sm:pt-4">
+      <div class="text-darkblue font-cooper-semibold text-base sm:text-2xl">{start}</div>
+      <a class="text-darkblue font-cooper-semibold text-sm sm:text-lg underline sm:pt-2" target="_blank" href="{link}">Link</a>
+    </div>
+  </div>
+</div>
+
+<style>
+.day {
+    width: 86px;
+    font-size: 72px;
+  }
+
+  .month {
+    width: 86px;
+    font-size: 46px;
+  }
+
+  @media (min-width: 640px) {
+    .day {
+      width: 140px;
+      font-size: 121px;
+    }
+  
+    .month {
+      width: 140px;
+      font-size: 77px;
+    }
+   }
+</style>
--- src/routes/section_concert.svelte
+++ src/routes/section_concert.svelte
@@ -2,6 +2,12 @@
   import SectionSlantedTop from "../components/layout/section_slanted_top.svelte";
   import SectionTitle from "../components/layout/section_title.svelte";
   import SectionContent from "../components/layout/section_content.svelte";
+  import Concert from "../components/concert.svelte";
+
+  let concerts_2022 = [
+		{ date: new Date(), titel: 'Kellertheater Wangenstrasse 45, 4537 Wiedlisbach', start: 'Beginn, ca. 20:00 Uhr', link: 'www.beleven.ch' },
+		{ date: new Date(), titel: 'Solothurner Musiktage', start: 'Beginn, ca. 21:00 Uhr', link: 'www.beleven.ch' },
+	];
 </script>
 
 <section id="concert" class="relative z-0 bg-orange">
@@ -10,7 +16,11 @@
   <SectionContent>
     <SectionTitle colorName='darkblue'>Konzerte</SectionTitle>
 
-    <div class="h-96 border-2 text-white text-center"><p>Placeholder</p></div>
+    <div class="divide-y divide-white/40">
+      {#each concerts_2022 as concert}
+        <Concert date={concert.date} titel={concert.titel} start={concert.start} link={concert.link}/>
+      {/each}
+    </div>
   </SectionContent>
 </section>
 
