c2f22f139b4854060e64f4fcc46ba042a111a88b
u215084 <daniel.fink@sbb.ch>
2022-10-20 21:18:14 +0200

    autoformat

--- src/routes/section_band.svelte
+++ src/routes/section_band.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import SectionSlantedTop from "../components/layout/section_slanted_top.svelte";
-  import SectionTitle from "../components/layout/section_title.svelte";
-  import SectionContent from "../components/layout/section_content.svelte";
-  import Avatar from "../components/avatar.svelte";
+  import SectionSlantedTop from '../components/layout/section_slanted_top.svelte';
+  import SectionTitle from '../components/layout/section_title.svelte';
+  import SectionContent from '../components/layout/section_content.svelte';
+  import Avatar from '../components/avatar.svelte';
 </script>
 
 <section id="band" class="relative z-0 bg-darkgray">
@@ -18,7 +18,7 @@
         <Avatar name="Marc Fischer" instrument="Vocals & Guitar" image_src="/assets/maercu_f.png" />
         <Avatar name="Daniel Fink" instrument="Guitar & Backing Vocals" image_src="/assets/dani.png" />
       </div>
-  
+
       <div class="flex justify-evenly px-32">
         <Avatar name="Markus Günter" instrument="Bass & Backing Vocals" image_src="/assets/kusi.png" />
         <Avatar name="Marc Leuenberger" instrument="Drums" image_src="/assets/maercu_l.png" />
@@ -28,12 +28,11 @@
     <!-- Tablet, Mobile -->
     <div class="md:hidden flex flex-col items-center">
       <Avatar name="Roman Bürki" instrument="Piano & Keyboards" image_src="/assets/roemu.png" />
-        <Avatar name="Marc Fischer" instrument="Vocals & Guitar" image_src="/assets/maercu_f.png" />
-        <Avatar name="Daniel Fink" instrument="Guitar & Backing Vocals" image_src="/assets/dani.png" />
+      <Avatar name="Marc Fischer" instrument="Vocals & Guitar" image_src="/assets/maercu_f.png" />
+      <Avatar name="Daniel Fink" instrument="Guitar & Backing Vocals" image_src="/assets/dani.png" />
       <Avatar name="Markus Günter" instrument="Bass & Backing Vocals" image_src="/assets/kusi.png" />
       <Avatar name="Marc Leuenberger" instrument="Drums" image_src="/assets/maercu_l.png" />
     </div>
-    
   </SectionContent>
 </section>
 
