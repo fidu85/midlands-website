fc70e80dd57262bb3a7ec8fe5a765e0c1abb913e
u215084 <daniel.fink@sbb.ch>
2022-10-20 21:16:57 +0200

    avatar responsive correction

--- src/components/avatar.svelte
+++ src/components/avatar.svelte
@@ -4,12 +4,12 @@
   export let instrument: string = 'Instrument';
 </script>
 
-<div class="w-60 md:w-52 lg:w-60 flex flex-wrap justify-center space-y-4 pb-12 hover:scale-105 transition duration-700 ease-in-out">
-  <img src={image_src} alt="bandmitglied" class="h-60 md:h-52 lg:h-60 drop-shadow-md rounded-3xl -rotate-3 border-none bg-gradient-to-b from-midgray to-darkgray grayscale hover:grayscale-0 hover:-rotate-6 transition duration-700" />
-  <span class="absolute -z-10 h-60 md:h-52 lg:h-60 w-60 md:w-52 lg:w-60 rounded-3xl rotate-3 bg-orange/75 -translate-y-4" />
+<div class="w-52 sm:w-60 md:w-48 lg:w-60 flex flex-wrap justify-center space-y-4 pb-14 sm:pb-20 md:pb-12 hover:scale-105 transition duration-700 ease-in-out">
+  <img src={image_src} alt="bandmitglied" class="h-52 sm:h-60 md:h-48 lg:h-60 drop-shadow-md rounded-3xl -rotate-3 border-none bg-gradient-to-b from-midgray to-darkgray grayscale hover:grayscale-0 hover:-rotate-6 transition duration-700" />
+  <span class="absolute -z-10 h-52 sm:h-60 md:h-48 lg:h-60 w-52 sm:w-60 md:w-48 lg:w-60 rounded-3xl rotate-3 bg-orange/75 -translate-y-4" />
   <span class="text-white flex flex-col space-y-1 items-center text-center">
-    <p class="font-cooper-medium text-base lg:text-xl tracking-wider uppercase">{name}</p>
-    <p class="font-cooper-light text-base lg:text-lg tracking-wider uppercase">{instrument}</p>
+    <p class="font-cooper-medium text-base sm:text-xl md:text-lg lg:text-xl tracking-wider uppercase">{name}</p>
+    <p class="font-cooper-light text-base sm:text-lg md:text-base lg:text-lg tracking-wider uppercase">{instrument}</p>
   </span>
 </div>
 
