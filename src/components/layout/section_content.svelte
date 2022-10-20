164d451009fd9abff3bfc353dea0f00867d1e6db
u215084 <daniel.fink@sbb.ch>
2022-10-20 21:17:54 +0200

    conent section responsive

--- src/components/layout/section_content.svelte
+++ src/components/layout/section_content.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="max-w-screen-2xl mx-auto px-4 sm:px-6 pb-36 sm:pb-72">
+<div class="max-w-screen-2xl mx-auto px-4 sm:px-6 pb-36 sm:pb-48 md:pb-72">
   <slot />
 </div>
 
