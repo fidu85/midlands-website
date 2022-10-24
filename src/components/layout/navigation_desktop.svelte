873f67067fccfcbd96753a3038d9262ea3690b00
u215084 <daniel.fink@sbb.ch>
2022-10-24 18:11:41 +0200

    smooth scrolling for navigation

--- src/components/layout/navigation_desktop.svelte
+++ src/components/layout/navigation_desktop.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <nav class="fixed w-full z-10">
-  <div class="h-24 max-w-screen-2xl mx-auto px-4 sm:px-6 flex items-center justify-end gap-16 text-white font-cooper-light tracking-widest text-2xl">
+  <div class="h-24 max-w-screen-2xl mx-auto px-4 sm:px-6 flex items-center justify-end gap-16 text-white font-cooper-light tracking-widest text-2xl drop-shadow-lg">
     <a href="/#band" class="hover:translate-y-2 duration-300">BAND</a>
     <a href="/#concert" class="hover:translate-y-2 duration-300">KONZERTE</a>
     <a href="/#music" class="hover:translate-y-2 duration-300">MUSIK</a>
