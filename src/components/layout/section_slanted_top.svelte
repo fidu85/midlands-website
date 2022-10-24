3f7053359cb432cac486655beef0f60b24acc1ea
u215084 <daniel.fink@sbb.ch>
2022-10-24 14:59:17 +0200

    change slanted top height for 4K

--- src/components/layout/section_slanted_top.svelte
+++ src/components/layout/section_slanted_top.svelte
@@ -7,18 +7,18 @@
   class="before:content-[''] 
           before:block 
           before:w-full 
-          before:h-36 
+          before:h-1/4
           {isTopRight ? 'before:-skew-y-3' : 'before:skew-y-3'}
           {isTopRight ? 'before:origin-left' : 'before:origin-right'}
           before:absolute 
           before:top-0 
           {isTopRight ? 'before:left-0' : 'before:right-0'}
           before:-z-10 
-          {colorName=='orange' ? 'before:bg-orange': ''} 
-          {colorName=='white' ? 'before:bg-white': ''} 
-          {colorName=='darkgray' ? 'before:bg-darkgray': ''} 
-          {colorName=='midgray' ? 'before:bg-midgray': ''} 
-          {colorName=='darkblue' ? 'before:bg-darkblue': ''}"
+          {colorName == 'orange' ? 'before:bg-orange' : ''} 
+          {colorName == 'white' ? 'before:bg-white' : ''} 
+          {colorName == 'darkgray' ? 'before:bg-darkgray' : ''} 
+          {colorName == 'midgray' ? 'before:bg-midgray' : ''} 
+          {colorName == 'darkblue' ? 'before:bg-darkblue' : ''}"
 />
 
 <style>
