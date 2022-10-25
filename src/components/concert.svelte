29b1c34489ced48fdd84da7b05046c11dd4b9a9d
u215084 <daniel.fink@sbb.ch>
2022-10-25 18:15:05 +0200

    first load concert from concert.json

--- src/components/concert.svelte
+++ src/components/concert.svelte
@@ -2,30 +2,26 @@
   export let date: Date = new Date();
   export let titel: String = 'Titel';
   export let start: String = 'Start';
-  export let link: string = 'https://www.midlands.ch'
-
-  const monthNames = ["JAN", "FEB", "MAR", "APR", "MAI", "JUN",
-  "JUL", "AUG", "SEP", "OKT", "NOV", "DEZ"];
-
+  export let link: string = 'https://www.midlands.ch';
 </script>
 
 <div class="py-3 sm:py-6 sm:px-12">
   <div class="flex flex-row items-end space-x-6 sm:space-x-12">
-    <div class="day text-white font-cooper-heavy opacity-40">{date.getDate()}</div>
+    <div class="day text-white font-cooper-heavy opacity-40">{new Intl.DateTimeFormat('de', { day: '2-digit' }).format(date)}</div>
     <div class="text-darkblue font-cooper-bold text-lg leading-6 sm:text-4xl pb-7 sm:pb-12">{titel}</div>
   </div>
-  
+
   <div class="flex flex-row items-start space-x-6 sm:space-x-12 -mt-8 sm:-mt-14">
-    <div class="month text-white font-cooper-heavy opacity-40">{monthNames[date.getMonth()]}</div>
+    <div class="month text-white font-cooper-heavy opacity-40 uppercase">{new Intl.DateTimeFormat('de', { month: 'short' }).format(date)}</div>
     <div class="flex flex-col justify-between pt-2 sm:pt-4">
       <div class="text-darkblue font-cooper-semibold text-base sm:text-2xl">{start}</div>
-      <a class="text-darkblue font-cooper-semibold text-sm sm:text-lg underline sm:pt-2" target="_blank" href="{link}">Link</a>
+      <a class="text-darkblue font-cooper-semibold text-sm sm:text-lg underline sm:pt-2" target="_blank" href={link}>Link</a>
     </div>
   </div>
 </div>
 
 <style>
-.day {
+  .day {
     width: 86px;
     font-size: 72px;
   }
@@ -40,10 +36,10 @@
       width: 140px;
       font-size: 121px;
     }
-  
+
     .month {
       width: 140px;
       font-size: 77px;
     }
-   }
+  }
 </style>
--- src/routes/section_concert.svelte
+++ src/routes/section_concert.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
-  import SectionSlantedTop from "../components/layout/section_slanted_top.svelte";
-  import SectionTitle from "../components/layout/section_title.svelte";
-  import SectionContent from "../components/layout/section_content.svelte";
-  import Concert from "../components/concert.svelte";
+  import SectionSlantedTop from '../components/layout/section_slanted_top.svelte';
+  import SectionTitle from '../components/layout/section_title.svelte';
+  import SectionContent from '../components/layout/section_content.svelte';
+  import Concert from '../components/concert.svelte';
 
-  let concerts_2022 = [
-		{ date: new Date(), titel: 'Kellertheater Wangenstrasse 45, 4537 Wiedlisbach', start: 'Beginn, ca. 20:00 Uhr', link: 'www.beleven.ch' },
-		{ date: new Date(), titel: 'Solothurner Musiktage', start: 'Beginn, ca. 21:00 Uhr', link: 'www.beleven.ch' },
-	];
+  import concerts from '../data/concerts.json';
+  let sortedConcerts = concerts.sort((objA, objB) => new Date(objB.date).getTime() - new Date(objA.date).getTime());
 </script>
 
 <section id="concert" class="relative z-0 bg-orange">
   <SectionSlantedTop isTopRight={false} colorName="orange" />
 
   <SectionContent>
-    <SectionTitle colorName='darkblue'>Konzerte</SectionTitle>
+    <SectionTitle colorName="darkblue">Konzerte</SectionTitle>
 
     <div class="divide-y divide-white/40">
-      {#each concerts_2022 as concert}
-        <Concert date={concert.date} titel={concert.titel} start={concert.start} link={concert.link}/>
+      {#each sortedConcerts as concert}
+        <Concert date={new Date(concert.date)} titel={concert.titel} start={concert.start} link={concert.link} />
       {/each}
     </div>
   </SectionContent>
