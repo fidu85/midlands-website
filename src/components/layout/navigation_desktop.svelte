7245e41cd47b7ee7a1a49b70ed886ebe1b172df0
u215084 <daniel.fink@sbb.ch>
2022-10-27 21:09:12 +0200

    prepare navigation for scroll-animation

--- src/components/layout/navigation_desktop.svelte
+++ src/components/layout/navigation_desktop.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 </script>
 
-<nav class="fixed w-full z-10">
+<!-- <nav class="fixed w-full z-10 backdrop-blur-sm shadow-lg bg-darkgray/60"> -->
+<nav class="fixed w-full z-10 ">
   <div class="h-24 max-w-screen-2xl mx-auto px-4 sm:px-6 flex items-center justify-end gap-16 text-white font-cooper-light tracking-widest text-2xl drop-shadow-lg">
-    <a href="/#band" class="hover:translate-y-2 duration-300">BAND</a>
-    <a href="/#concert" class="hover:translate-y-2 duration-300">KONZERTE</a>
-    <a href="/#music" class="hover:translate-y-2 duration-300">MUSIK</a>
-    <a href="/#promoter" class="hover:translate-y-2 duration-300">VERANSTALTER</a>
-    <a href="/#contact" class="hover:translate-y-2 duration-300">KONTAKT</a>
+    <a href="/#band" class="hover:translate-y-2 hover:text-orange duration-300">BAND</a>
+    <a href="/#concert" class="hover:translate-y-2 hover:text-orange duration-300">KONZERTE</a>
+    <!-- <a href="/#music" class="hover:translate-y-2 hover:text-orange duration-300">MUSIK</a>
+    <a href="/#promoter" class="hover:translate-y-2 hover:text-orange duration-300">VERANSTALTER</a> -->
+    <a href="/#contact" class="hover:translate-y-2 hover:text-orange duration-300">KONTAKT</a>
   </div>
 </nav>
 
