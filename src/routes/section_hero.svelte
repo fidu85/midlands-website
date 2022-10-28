3d128c1927f3c587e1840583ffbbeb437699e049
u215084 <daniel.fink@sbb.ch>
2022-10-28 17:48:31 +0200

    hero first scroll animation

--- src/routes/section_hero.svelte
+++ src/routes/section_hero.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
   import SectionContent from '../components/layout/section_content.svelte';
+  import { scale } from 'svelte/transition';
+  import { bounceOut, circOut } from 'svelte/easing';
+
+  let currentScroll: number;
+  let viewportHeight: number;
 </script>
+
+<svelte:window bind:scrollY={currentScroll} bind:innerHeight={viewportHeight} />
 
 <section id="hero" class="relative bg-hero-background bg-center bg-no-repeat min-h-screen portrait:min-h-[calc(80vh)]">
   <SectionContent>
-    <img src="assets/midlands_logo.svg" alt="midlands logo" class="logo absolute max-w-screen-2xl mx-auto px-4 sm:px-6 w-full top-[calc(20%)] portrait:top-1/3" />
-    <img src="assets/midlands_band.png" alt="midlands band" class="band absolute max-w-screen-2xl mx-auto px-4 sm:px-6 w-full landscape:w-2/3 portrait:md:w-full landscape:md:w-2/3" />
+    <img src="/assets/midlands_logo.svg" alt="midlands logo" class="logo fixed max-w-screen-2xl mx-auto px-4 sm:px-6 w-full top-[calc(20%)] portrait:top-1/3" />
+    <img src="/assets/midlands_band.png" alt="midlands band" class="band absolute max-w-screen-2xl mx-auto px-4 sm:px-6 w-full landscape:w-2/3 portrait:md:w-full landscape:md:w-2/3" style:transform={`translate3d(0, ${currentScroll * 0.3}px, 0)`} />
   </SectionContent>
 </section>
 
