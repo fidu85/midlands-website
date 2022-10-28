63b5bfb8859053d8655395b8f512f5d34c43d959
u215084 <daniel.fink@sbb.ch>
2022-10-28 18:04:24 +0200

    better animation for navbar desktop

--- src/components/layout/navigation_desktop.svelte
+++ src/components/layout/navigation_desktop.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { slide } from 'svelte/transition';
-  import { bounceOut, circOut } from 'svelte/easing';
+  import { bounceIn, bounceOut, circOut, elasticOut } from 'svelte/easing';
   let currentScroll: number;
   let beforeScroll: number;
 
@@ -17,13 +17,13 @@
 <svelte:window bind:scrollY={currentScroll} />
 
 {#if showNav}
-  <nav class="fixed w-full z-10 {currentScroll < 96 ? '' : 'backdrop-blur-sm shadow-lg bg-darkgray/20'}" out:slide={{ easing: circOut, duration: 1000 }} in:slide={{ easing: bounceOut, duration: 1000 }}>
+  <nav class="fixed w-full z-10 {currentScroll < 96 ? '' : 'backdrop-blur-sm shadow-lg bg-darkgray/20'}" out:slide={{ easing: circOut, duration: 500 }} in:slide={{ easing: bounceOut, duration: 1000 }}>
     <div class="h-24 max-w-screen-2xl mx-auto px-4 sm:px-6 flex items-center justify-end gap-16 text-white font-cooper-light tracking-widest text-2xl drop-shadow-lg">
-      <a href="/#band" class="hover:translate-y-2 hover:text-orange duration-300">BAND</a>
-      <a href="/#concert" class="hover:translate-y-2 hover:text-orange duration-300">KONZERTE</a>
+      <a href="/#band" class="hover:translate-y-2 hover:text-orange duration-300" in:slide={{ easing: elasticOut, duration: 1000, delay: 300 }}>BAND</a>
+      <a href="/#concert" class="hover:translate-y-2 hover:text-orange duration-300" in:slide={{ easing: elasticOut, duration: 1000, delay: 500 }}>KONZERTE</a>
       <!-- <a href="/#music" class="hover:translate-y-2 hover:text-orange duration-300">MUSIK</a>
     <a href="/#promoter" class="hover:translate-y-2 hover:text-orange duration-300">VERANSTALTER</a> -->
-      <a href="/#contact" class="hover:translate-y-2 hover:text-orange duration-300">KONTAKT</a>
+      <a href="/#contact" class="hover:translate-y-2 hover:text-orange duration-300" in:slide={{ easing: elasticOut, duration: 1000, delay: 700 }}>KONTAKT</a>
     </div>
   </nav>
 {/if}
